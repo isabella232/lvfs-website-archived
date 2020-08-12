16c6478b2157a3fa29ba8ec8fa31468b18b35c40
Richard Hughes <richard@hughsie.com>
2020-08-12 19:02:40 +0100

    Do not configure celery during init
    This allows us to run local.py without redis running.

--- lvfs/metadata/routes.py
+++ lvfs/metadata/routes.py
@@ -19,9 +19,6 @@ from .utils import _async_regenerate_remote_all, _async_regenerate_remote
 
 bp_metadata = Blueprint('metadata', __name__, template_folder='templates')
 
-celery.control.time_limit('lvfs.metadata.utils._async_regenerate_remote',
-                          soft=60, hard=120, reply=True)
-
 @celery.on_after_finalize.connect
 def setup_periodic_tasks(sender, **_):
     sender.add_periodic_task(14400.0, _async_regenerate_remote_all.s(), options={'queue': 'metadata'})
