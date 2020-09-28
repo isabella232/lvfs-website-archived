ea3853c55c8f9269239a5530cc5e3b3d35b63391
lfdwt <dtalton@contractor.linuxfoundation.org>
2020-09-28 19:51:56 +0000

    Update lvfs-entrypoint.sh

--- docker/files/lvfs-entrypoint.sh
+++ docker/files/lvfs-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-if [ $DEPLOY == "application" ]
+if [ "$DEPLOY" = "application" ]
 then
 	exec uwsgi --ini /app/conf/uwsgi.ini
 fi
 
-if [ $DEPLOY == "worker" ]
+if [ "$DEPLOY" == "worker" ]
 	exec celery -A lvfs.celery.worker --queues metadata,firmware,celery,yara
 fi
