d90aa83a1d06986b338b3ff60a5346179dfbf70a
Richard Hughes <richard@hughsie.com>
2020-10-06 10:00:42 +0100

    Ensure all for elements use for=""` correctly
    Also, ensure all images have valid `alt=` data for screen readers.
    
    For my future self: `name=` is for the form data, `id=` is used for the DOM.

--- lvfs/claims/routes.py
+++ lvfs/claims/routes.py
@@ -106,4 +106,5 @@ def route_show(claim_id):
 
     # show details
     return render_template('claim-details.html',
+                           category='admin',
                            claim=claim)
