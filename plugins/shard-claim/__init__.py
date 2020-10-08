9a23c9db867fd2795041f10b7936e7f7189d6dfa
Richard Hughes <richard@hughsie.com>
2020-10-08 10:59:30 +0100

    Simplify the shard-claim plugin
    Hopefully this fixes the 'ComponentShardInfo is not bound to a Session' error
    message which occasionally pops up.

--- plugins/shard-claim/__init__.py
+++ plugins/shard-claim/__init__.py
@@ -15,8 +15,6 @@ from lvfs.models import Test, ComponentShard, ComponentShardInfo, \
 class Plugin(PluginBase):
     def __init__(self, plugin_id=None):
         PluginBase.__init__(self, plugin_id)
-        self.infos_by_guid = {}
-        self.claims_by_csum = {}
         self.name = 'Shard Claim'
         self.summary = 'Add component claims based on shard GUIDs'
         self.order_after = ['uefi-extract']
@@ -29,33 +27,26 @@ class Plugin(PluginBase):
             test = Test(plugin_id=self.id, waivable=True)
             fw.tests.append(test)
 
-    def run_test_on_fw(self, test, fw):
+    def run_test_on_md(self, test, md):
+
+        infos_by_guid = {}
+        claims_by_csum = {}
 
         # find any infos that indicate a claim
-        if not self.infos_by_guid:
-            for info in db.session.query(ComponentShardInfo)\
-                                  .filter(ComponentShardInfo.claim_id != None):
-                self.infos_by_guid[info.guid] = info
-        if not self.claims_by_csum:
-            for claim in db.session.query(ComponentShardClaim)\
-                                   .filter(ComponentShardClaim.checksum != None):
-                self.claims_by_csum[claim.checksum] = claim
-
-        # get cache of all known claims
-        claims = {}
-        for claim in db.session.query(Claim).all():
-            claims[claim.kind] = claim
-
-    def run_test_on_md(self, test, md):
+        for info in db.session.query(ComponentShardInfo)\
+                              .filter(ComponentShardInfo.claim_id != None):
+            infos_by_guid[info.guid] = info
+        for claim in db.session.query(ComponentShardClaim)\
+                               .filter(ComponentShardClaim.claim_id != None)\
+                               .filter(ComponentShardClaim.checksum != None):
+            claims_by_csum[claim.checksum] = claim
 
         # run analysis on the component and any shards
         for shard in md.shards:
-            if shard.guid in self.infos_by_guid:
-                info = self.infos_by_guid[shard.guid]
-                md.add_claim(info.claim)
-            if shard.checksum in self.claims_by_csum:
-                shard_claim = self.claims_by_csum[shard.checksum]
-                md.add_claim(shard_claim.claim)
+            if shard.guid in infos_by_guid:
+                md.add_claim(infos_by_guid[shard.guid].claim)
+            if shard.checksum in claims_by_csum:
+                md.add_claim(claims_by_csum[shard.checksum].claim)
 
 # run with PYTHONPATH=. ./env/bin/python3 plugins/shard-claim/__init__.py
 if __name__ == '__main__':
@@ -72,7 +63,6 @@ if __name__ == '__main__':
                                                          'daccd169efcab451cbb32c5f45ef8a'))
     _md.shards.append(_shard)
     _fw.mds.append(_md)
-    plugin.run_test_on_fw(_test, _fw)
     plugin.run_test_on_md(_test, _md)
     for _claim in _md.claims:
         print(_claim)
