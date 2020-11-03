df772e4ba95bfa5ade49e72f0b981743ff0bc506
Richard Hughes <richard@hughsie.com>
2020-11-03 20:24:02 +0000

    trivial: Actually start the beat worker

--- docker/files/lvfs-entrypoint.sh
+++ docker/files/lvfs-entrypoint.sh
@@ -11,16 +11,20 @@ fi
 if [ "$DEPLOY" = "metadata" ]
 then
     set
+    echo "starting celery worker [metadata,firmware,celery]"
     exec celery -A lvfs.tq worker --queues metadata,firmware,celery
 fi
 
 if [ "$DEPLOY" = "yara" ]
 then
+    set
+    echo "starting celery worker [yara]"
     exec celery -A lvfs.tq worker --queues yara
 fi
 
 if [ "$DEPLOY" = "beat" ]
 then
-    PATH="/app/env/bin:$PATH"
-    FLASK_APP=lvfs/__init__.py celery -A lvfs.tq beat
+    set
+    echo "starting celery beat"
+    exec celery -A lvfs.tq beat
 fi
