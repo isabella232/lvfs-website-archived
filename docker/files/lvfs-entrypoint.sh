11f2ab1972f57406123452045007b0cc5b3913e7
Richard Hughes <richard@hughsie.com>
2020-11-04 19:00:56 +0000

    trivial: Increase the celery debug level

--- docker/files/lvfs-entrypoint.sh
+++ docker/files/lvfs-entrypoint.sh
@@ -11,19 +11,19 @@ fi
 if [ "$DEPLOY" = "metadata" ]
 then
     echo "starting celery worker [metadata,firmware,celery]"
-    exec celery -A lvfs.tq worker --queues metadata,firmware,celery
+    exec celery -A lvfs.tq worker --queues metadata,firmware,celery --loglevel INFO
 fi
 
 if [ "$DEPLOY" = "yara" ]
 then
     echo "starting celery worker [yara]"
-    exec celery -A lvfs.tq worker --queues yara
+    exec celery -A lvfs.tq worker --queues yara --loglevel INFO
 fi
 
 if [ "$DEPLOY" = "beat" ]
 then
     echo "starting celery beat"
-    exec celery -A lvfs.tq beat
+    exec celery -A lvfs.tq beat --loglevel INFO
 fi
 
 if [ "$DEPLOY" = "clam" ]
